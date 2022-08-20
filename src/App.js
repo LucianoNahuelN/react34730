78db0ff39187acd273dd41a66e67d6744f7a60c1
LucianoNahuelN <lucianonahuelnunez@gmail.com>
2022-08-20 14:21:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hola mundo</h1>
+    <h3>Otro texto</h3>
+    </>
+    
+  );
+}
+
+export default App;
